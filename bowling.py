f18266a4d75de8a034ed71d95ac5736ac067f4d0
Tamas Horvath <horvathtamas22@gmail.com>
2017-11-22 15:20:45 +0100

    all 'X' removed, compares only to 'x' , at the end of score() func setting is_first_try and frame is shorter now, in 'if frame < 10...' removed one of the 'result += get_value(game[i+1])', and refactored the inside 'if'

--- bowling.py
+++ bowling.py
@@ -1,45 +1,37 @@
 def score(game):
     result = 0
     frame = 1
-    in_first_try = True
+    is_first_try = True
+    last_score = 0
     for i in range(len(game)):
         if game[i] == '/':
-            result += 10 - last
+            result += 10 - last_score
         else:
             result += get_value(game[i])
         
-        if frame < 10  and get_value(game[i]) == 10:
-            if game[i] == '/':
-                result += get_value(game[i+1])
-            elif game[i] == 'X' or game[i] == 'x':
-                result += get_value(game[i+1])
+        if frame < 10  and (game[i].lower() == 'x' or game[i] == '/'):
+            result += get_value(game[i+1])
+            if game[i].lower() == 'x':
                 if game[i+2] == '/':
                     result += 10 - get_value(game[i+1])
                 else:
                     result += get_value(game[i+2])
                     
-        last = get_value(game[i])
-        #if in_first_try == False:
-        #    frame += 1
+        last_score = get_value(game[i])
 
-        if in_first_try == True:
-            in_first_try = False
-        else:
-            in_first_try = True
-            frame +=1
-        if game[i] == 'X' or game[i] == 'x':
-            in_first_try = True
+        if game[i].lower() == 'x' or is_first_try == False:
+            is_first_try = True
             frame += 1
+        elif is_first_try == True:
+            is_first_try = False
     return result
 
 def get_value(char):
     if char in [str(num) for num in range(1, 10)]:
         return int(char)
-    elif char == 'X' or char == 'x' or char == '/':
+    elif char.lower() == 'x' or char == '/':
         return 10
     elif char == '-':
         return 0
-    
         
-game = "5/11------------3/11"
-print(score(game))
+
