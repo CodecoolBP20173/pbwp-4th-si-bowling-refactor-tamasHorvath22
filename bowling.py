70c34ff9b1297e33d63271f5b3943ed9616f368d
Tamas Horvath <horvathtamas22@gmail.com>
2017-11-22 15:48:02 +0100

    the part which checks if there was a spare, scored out to a function called 'if_spare()', and called two times in score() function

--- bowling.py
+++ bowling.py
@@ -4,18 +4,12 @@ def score(game):
     is_first_try = True
     last_score = 0
     for i in range(len(game)):
-        if game[i] == '/':
-            result += 10 - last_score
-        else:
-            result += get_value(game[i])
+        result += strike_second(game[i], last_score)
         
         if frame < 10  and (game[i].lower() == 'x' or game[i] == '/'):
             result += get_value(game[i+1])
             if game[i].lower() == 'x':
-                if game[i+2] == '/':
-                    result += 10 - get_value(game[i+1])
-                else:
-                    result += get_value(game[i+2])
+                result += strike_second(game[i+2], get_value(game[i+1]))
                     
         last_score = get_value(game[i])
 
@@ -25,6 +19,13 @@ def score(game):
         elif is_first_try == True:
             is_first_try = False
     return result
+
+def strike_second(char, last_score):
+    if char == '/':
+        points = 10 - last_score
+    else:
+        points = get_value(char)
+    return points
 
 def get_value(char):
     if char in [str(num) for num in range(1, 10)]:
